9e5c753c9fe96a9e37af943302bbc220a7a11360
AntonioIndindoli <50554331+AntonioIndindoli@users.noreply.github.com>
2025-06-21 10:19:51 -0700

    Remove backend and update header

---
+++ src/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const About = () => (
+  <div className="about-page">
+    <h1>About</h1>
+    <p>This site showcases our projects and experiments.</p>
+  </div>
+);
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
-import { Container, Col, Row } from "react-bootstrap";
-import Register from "./Register";
-import Login from "./Login";
-import Logout from "./Logout";
+import { Col } from "react-bootstrap";
 import Home from "./Home";
 import LandingPage from "./LandingPage";
 import AlbumPage from "./AlbumPage";
-import './App.css';
-import FreeComponent from "./FreeComponent";
-import AuthComponent from "./AuthComponent";
-import Members from "./Members";
-import ProfileView from "./ProfileView";
-import PostComment from "./PostComment";
-import Comments from "./Comments";
-import MyProfile from "./MyProfile";
+import About from "./About";
+import Extras from "./Extras";
 import UnityDemo from "./UnityDemo";
-import DestructibleStructureBuilder from "./DestructibleStructureBuilder";
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-//import ProtectedRoutes from "./ProtectedRoutes";
-//import { Routes, Route, Navigate, useLocation } from "react-router-dom";
-import Cookies from "universal-cookie";
-import EditUser from "./EditUser";
-const cookies = new Cookies();
+import { Routes, Route } from "react-router-dom";
+import './App.css';
 
 function App() {
-  const ProtectedRoutes = ({ children }) => {
-    let location = useLocation();
-    const token = localStorage.getItem("TOKEN");
-
-    // returns route if there is a valid token set in the cookie
-    if (!token) {
-      return <Navigate to="/login" state={{ from: location }} />;
-    }
-
-    return children;
-  };
 
   return (
     <Col className="App">
       <Routes>
-      <Route exact path="/Album" element={<AlbumPage />} />
+        <Route exact path="/Album" element={<AlbumPage />} />
         <Route exact path="/Blog" element={<Home />} />
         <Route exact path="/" element={<LandingPage />} />
-        <Route path="/profile/:user" element={<ProfileView/>} />
-        <Route path="/PostComment/:postId" element={<PostComment/>} />
-        <Route path="/comment/:postId" element={<Comments/>} />
-        <Route path="/myProfile/:user" element={<MyProfile/>} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/logout" element={<Logout />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/free" element={<FreeComponent />} />
-        <Route exact path="/userlist" element={<Members />}/>
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/extras" element={<Extras />} />
         <Route exact path="/unity-demo" element={<UnityDemo />} />
-        <Route exact path="/destructible-structure-builder" element={<DestructibleStructureBuilder />} />
-        <Route exact path="/auth" element={<ProtectedRoutes><AuthComponent /></ProtectedRoutes>} />
       </Routes>
     </Col >
   );
